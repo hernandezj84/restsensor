d948c444598e2fc6827cfaa9d8835c3909c6e92d
Jonathan Hernandez <hernandezj84@gmail.com>
2020-06-17 17:07:29 -0500

    Add DEFAULT_RENDERER_CLASSES

--- restsensor/settings.py
+++ restsensor/settings.py
@@ -73,6 +73,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'restsensor.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',)
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
