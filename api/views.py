be4e9a3649cb20ce351b5e87369f4e2b43715866
Jonathan Hernandez <hernandezj84@gmail.com>
2020-06-17 11:12:40 -0500

    First test api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+
+@api_view(['GET'])
+def test(request):
+    data = {"message": "hello world from api"}
+    return Response(data)
