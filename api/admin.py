2b2c4f8e4f2d3a1157790f1b936f37301a1a022d
Jonathan Hernandez <hernandezj84@gmail.com>
2020-06-18 21:52:25 -0500

    Modify databases. Add DeviceUser, Device, Event. According to the README.MD document

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from api.models import ApiUser, Category, Device, Event, WifiGlp
+from api.models import ApiUser, Category, Device, DeviceUser, Alarm, Event, GasType, WifiGlp
 
 admin.site.register(ApiUser)
 admin.site.register(Category)
 admin.site.register(Device)
+admin.site.register(DeviceUser)
+admin.site.register(Alarm)
 admin.site.register(Event)
+admin.site.register(GasType)
 admin.site.register(WifiGlp)
--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class ApiUser(models.Model):
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     name = models.CharField(max_length=100)
@@ -12,7 +13,8 @@ class ApiUser(models.Model):
     country = models.CharField(max_length=100)
 
     def __str__(self):
-        return "{} | {} {} | {}".format(self.user.email, self.name, self.lastname, self.phone)
+        return "{} | {} {} | {}".format(self.user, self.name, self.lastname, self.phone)
+
 
 class Category(models.Model):
     class Meta:
@@ -23,34 +25,60 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Device(models.Model):
     class Meta:
         verbose_name_plural = "devices"
-    
+    device_id = models.CharField(max_length=20, unique=True)
     device_type = models.ForeignKey(Category, on_delete=models.PROTECT)
-    serial_number = models.CharField(max_length=100, unique=True)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    serial = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
-        return "{} | {}".format(self.device_type.name, self.serial_number)
+        return "{} | {}".format(self.device_id, self.device_type)
+
+
+class DeviceUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    device = models.ForeignKey(Device, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return "{} | {}".format(self.user, self.device)
+
+
+class Alarm(models.Model):
+    alarm = models.CharField(max_length=100)
+    description = models.CharField(max_length=25)
+
+    def __str__(self):
+        return "{} | {}".format(self.alarm, self.description)
+
 
 class Event(models.Model):
     device = models.ForeignKey(Device, on_delete=models.PROTECT)
-    serial = models.CharField(max_length=100)
-    alarm = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    alarm = models.ForeignKey(Alarm, on_delete=models.PROTECT)
+    acc_time = models.CharField(max_length=100)
+    timestamp = models.CharField(max_length=100)
+    concent_gas = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
 
     def __str__(self):
-        return "{}-{}-{}".format(self.device, self.serial, self.alarm)
+        return "{}-{}-{}-{}".format(self.device, self.alarm, self.timestamp, self.status)
+
+
+class GasType(models.Model):
+    gas_type = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.gas_type
+
 
 class WifiGlp(models.Model):
     device = models.ForeignKey(Device, on_delete=models.PROTECT)
-    description = models.TextField()
-    ppm = models.CharField(max_length=100)
-    acc = models.CharField(max_length=100)
+    concent_gas = models.CharField(max_length=100)
+    acc_time = models.CharField(max_length=100)
     timestamp = models.CharField(max_length=100)
-    alarm = models.CharField(max_length=100)
-    gas_type = models.CharField(max_length=100)
+    alarm = models.ForeignKey(Alarm, on_delete=models.PROTECT)
+    gas_type = models.ForeignKey(GasType, on_delete=models.PROTECT)
 
     def __str__(self):
         return "{} | {} | {} | {}".format(self.device, self.timestamp, self.alarm, self.gas_type)
