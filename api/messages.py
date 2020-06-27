f799d37d7387f2aef5975b5346a3a01c7d906eb3
Jonathan Hernandez <hernandezj84@gmail.com>
2020-06-26 20:46:08 -0500

    Fix error messages

--- api/messages.py
+++ api/messages.py
@@ -1,12 +1,27 @@
-"""Module for success messages"""
+"""Modules that handles messages"""
 
 
 class SuccessMessages:
-    """Object with success messages"""
+    """Success Messages control"""
 
     def __init__(self):
         self.message = "success"
 
     def inserted(self, *args):
-        """Inserted message """
+        """Message when is a successfull insert"""
         return "{} inserted successfully".format(" ".join(args))
+
+
+class ErrorsMessages:
+    """Error messages control"""
+
+    def __init__(self):
+        self.message = "error"
+        self.invalid_post = "json post invalid according to the contract"
+        self.integrity_error = "Fields doesn't corresponds to database"
+        self.json_keys = "The json post's keys are invalid according the contract"
+        self.json_types = "The json post value's types are invalid according the contract"
+
+    def not_found(self, name):
+        """Error message when api doesn't found something"""
+        return "{} not found".format(name)
