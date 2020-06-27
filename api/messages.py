bdb9341923090cdeb253ca087ea973f8d4e2ca62
Jonathan Hernandez <hernandezj84@gmail.com>
2020-06-26 20:41:32 -0500

    Delete errors message

--- api/messages.py
+++ api/messages.py
@@ -1,21 +1,12 @@
-from rest_framework import status
+"""Module for success messages"""
 
 
 class SuccessMessages:
+    """Object with success messages"""
+
     def __init__(self):
         self.message = "success"
 
     def inserted(self, *args):
+        """Inserted message """
         return "{} inserted successfully".format(" ".join(args))
-
-
-class ErrorsMessages:
-    def __init__(self):
-        self.message = "error"
-        self.invalid_post = "json post invalid according to the contract"
-        self.integrity_error = "Fields doesn't corresponds to database"
-        self.json_keys = "The json post's keys are invalid according the contract"
-        self.json_types = "The json post value's types are invalid according the contract"
-
-    def not_found(self, name):
-        return "{} not found".format(name)
